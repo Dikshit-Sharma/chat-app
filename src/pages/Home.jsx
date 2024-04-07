a10291dc6d2d860a25aa3407b062d1dc2f2c4c39
Dikshit-Sharma <dikshit.sharma2580@gmail.com>
2024-04-07 11:55:34 +0530

    new file:   .firebase/hosting.YnVpbGQ.cache 	new file:   .firebaserc 	modified:   package.json 	new file:   src/components/Sidebar.jsx 	modified:   src/pages/Home.jsx 	modified:   src/style.scss 	modified:   yarn.lock

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
   return (
     <div className='home'>
       <div className="container">
-        <Navbar/>
+        <Sidebar/>
       </div>
     </div>
   )
